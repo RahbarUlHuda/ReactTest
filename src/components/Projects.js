b20a30461a000f6dda3f428cbc5944864223f447
RahbarUlHuda <rahbarulhuda32004@gmail.com>
2025-01-02 11:46:08 +0000

    Edited the Projects list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,13 @@ function Projects() {
       <ul>
         <li>
           <h3>Project 1</h3>
-          <p>Description of Project 1.</p>
+          <p>Web Innovators Battle ~ October 2024</p>
+          <p>Designed a website as part of a mandatory competition for the Introduction to AI and Data Science using Python course. Developed sections including About Me, Educational Background, Projects, and more, utilizing Generative AI tools for creative content.</p>
         </li>
         <li>
           <h3>Project 2</h3>
-          <p>Description of Project 2.</p>
+          <p>ReactTest</p>
+          <p>I created a personal portfolio website with the help of GitHub Codespaces, generative AI, and React. This project allowed me to gain a foundational understanding of React, including creating and managing components, implementing navigation, and styling</p>
         </li>
       </ul>
     </section>
