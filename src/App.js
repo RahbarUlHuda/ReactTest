4b8fc48e92bed2772c600c3364aa360d7c293353
RahbarUlHuda <rahbarulhuda32004@gmail.com>
2025-01-02 11:58:02 +0000

    Updated App.js for Details section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Details from './components/Details';
 import Contact from './components/Contact';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <About />
+      <Details />
       <Projects />
       <Contact />
     </div>
