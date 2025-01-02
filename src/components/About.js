3dd93fadceabbd5a920be3e837401eb4b44cea0d
RahbarUlHuda <rahbarulhuda32004@gmail.com>
2025-01-02 11:38:36 +0000

    Edited the About Me Text

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,8 @@ function About() {
   return (
     <section id="about">
       <h2>About Me</h2>
-      <p>Hi, I'm a passionate developer excited about creating impactful solutions!</p>
+      <p>I'm Rahbar Ul Huda A first-year B.Tech Computer Science & Engineering undergraduate with a deep passion for artificial intelligence, machine learning, and innovation. Currently, I’m building foundational skills in Python, Data Structures, and Algorithm Design, while also exploring development. I've had experience with Java during my school years and am committed to expanding my technical expertise to bring complex ideas to life.</p>
+      <p>I aim to combine my technical knowledge, building projects that leverage AI for real-world impact. I envision a future where my work pushes the boundaries of what's possible, applying cutting-edge AI advancements to make life more efficient, insightful, and accessible.</p>
     </section>
   );
 }
